2cec94bf41aeb547bfd78e3ff9bd3876c433ea74
Aviellevii <aviellevii97@gmail.com>
2022-06-05 23:17:39 +0300

    Add signin and login and adding auth

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,14 @@
 const express = require('express')
 const app = express();
 const cors = require('cors')
-
+const bcrypt = require('bcrypt');
 const bodyParser = require('body-parser')
-
+const jwt = require('jsonwebtoken');
 const mongoose = require('mongoose');
 const List = require('./model/list');
 const Task =require('./model/task');
+const User = require('./model/user');
+const checkAuth = require('./middlewares/checkAuth');
 app.use(bodyParser.json());
 
 app.use(cors());
@@ -20,13 +22,13 @@ mongoose.connection.on('connected', () => {
     console.log('mongoDB connected');
 })
 
-app.get('/lists',(req,res) => {
+app.get('/lists',checkAuth,(req,res) => {
     List.find().then((lists)=>{
         res.send(lists)
     })
 })
 
-app.post('/lists',(req,res) => {
+app.post('/lists',checkAuth,(req,res) => {
     const {title} = req.body;
 
     let newList = new List({
@@ -38,10 +40,10 @@ app.post('/lists',(req,res) => {
 
 })
 
-app.patch('/lists/:id',(req,res)=>{
+app.patch('/lists/:id',checkAuth,(req,res)=>{
     List.findOneAndUpdate({_id:req.params.id},{$set:req.body}).then(()=>res.sendStatus(200))
 })
-app.delete('/lists/:id',(req,res) => {
+app.delete('/lists/:id',checkAuth,(req,res) => {
     List.findOneAndRemove({_id:req.params.id}).then((listRemoved)=>{
         res.send(listRemoved)
     })
@@ -49,12 +51,12 @@ app.delete('/lists/:id',(req,res) => {
 
 
 
-app.get('/lists/:id/tasks',(req,res) => {
+app.get('/lists/:id/tasks',checkAuth,(req,res) => {
     Task.find({_listId:req.params.id}).then((tasks) => {
         res.send(tasks)
     })
 })
-app.get('/lists/:id/tasks/:taskId',(req,res) => {
+app.get('/lists/:id/tasks/:taskId',checkAuth,(req,res) => {
     Task.findOne({
         _id:req.params.taskId,
         _listId:req.params.id
@@ -62,7 +64,7 @@ app.get('/lists/:id/tasks/:taskId',(req,res) => {
         res.send(task)
     })
 })
-app.post('/lists/:id/tasks',(req,res) => {
+app.post('/lists/:id/tasks',checkAuth,(req,res) => {
     const {title} = req.body;
     
     let newTask = new Task({
@@ -74,7 +76,7 @@ app.post('/lists/:id/tasks',(req,res) => {
     })
 })
 
-app.patch('/lists/:id/tasks/:taskId',(req,res) => {
+app.patch('/lists/:id/tasks/:taskId',checkAuth,(req,res) => {
     Task.findByIdAndUpdate({
         _id:req.params.taskId,
         _listId:req.params.id
@@ -85,12 +87,67 @@ app.patch('/lists/:id/tasks/:taskId',(req,res) => {
     })
 })
 
-app.delete('/lists/:id/tasks/:taskId',(req,res) => {
+app.delete('/lists/:id/tasks/:taskId',checkAuth,(req,res) => {
     Task.findByIdAndRemove({
         _id:req.params.taskId,
         _listId:req.params.id
     }).then((taskDelete) => {
         res.send(taskDelete)
+    })
+})
+
+app.post('/user/regiser',(req,res) =>{
+    const {username,password} = req.body
+    bcrypt.hash(password,10,(err,hash) => {
+        const newUser = new User({
+            username,
+            password:hash
+        })
+        newUser.save().then((user)=>{
+            console.log(user)
+            res.send({message:'user Saved'})
+        })
+    })
+
+})
+
+app.post('/user/login',(req,res)=>{
+    const {username,password} = req.body;
+    User.find({username}).then((users)=>{
+        if(users.length === 0){
+            return res.status(401).json({
+                message:'Auth Failed'
+            })
+        }
+        const [user] = users;
+        bcrypt.compare(password,user.password,(err,result) => {
+            if(err){
+                return res.status(401).json({
+                    message:'Auth Failed'
+                })
+            }
+            if(result){
+                const token = jwt.sign({
+                    _id:user._id,
+                    username:user.username
+                },
+                    process.env.Key_JWT,
+                    {
+                        expiresIn:"1H"
+                    })
+                return res.status(200).json({
+                    message:'Auth Success',
+                    token,
+                    user:{
+                      id: user._id,
+                      username: user.username
+                }
+             })
+            }
+            res.status(401).json({
+                message:'Auth Failed'
+            })
+        })
     })
 })
 app.listen(3000,() => {
